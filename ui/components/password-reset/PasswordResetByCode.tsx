bf0c37ee3c1f39317ccc70401a5d41e3b14f8e8b
Derrick Munkhbat <derrick.munkhbat@gmail.com>
2024-03-20 19:14:42 +0800

    password-reset components
    password reset folder
     -by email
     -by code
     -new password

---
+++ ui/components/password-reset/PasswordResetByCode.tsx
@@ -0,0 +1,11 @@
+export default function PasswordResetByCode() {
+
+    return (
+      <div>
+        <h1>Нууц үг сэргээх код</h1>
+        <p>Имэйл </p>
+        <input type="text" placeholder="example@pinecone.mn"/><br/>
+        <button className="bg-green">Continue</button>
+      </div>
+    );
+  }
---
+++ ui/components/password-reset/PasswordResetNewPassword.tsx
@@ -0,0 +1,12 @@
+export default function PasswordResetNewPassword() {
+
+    return (
+      <div>
+        <h1>Нууц үг</h1>
+        <input type="text" placeholder="example@pinecone.mn"/><br/>
+        <h1>Нууц үг давтах</h1>
+        <input type="text" placeholder="example@pinecone.mn"/><br/>
+        <button className="bg-green">Continue</button>
+      </div>
+    );
+  }
