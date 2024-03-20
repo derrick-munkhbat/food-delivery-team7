f42768e46609d795c9d63cff37dfd2ae007d0da8
Amgalanbaatar <amgaabra08@gmail.com>
2024-03-20 20:06:48 +0800

    header front end done

--- ui/src/components/Header.tsx
+++ ui/src/components/Header.tsx
@@ -1,11 +1,33 @@
+import { HeaderLogo } from "../../public/Headerlogo";
+import { NevtrehIcon } from "../../public/Nevtrehicon";
+import { SagsIcon } from "../../public/SagsIcon";
+import { SearchIcon } from "../../public/SearchIcon";
 
-import { HeaderLogo } from "../../public/Headerlogo"
-
-export function Header (){
-
-    return (
-        <div className="top-0 z-10 bg-white  justify-between flex gap-5 container mx-auto border px-5 py-5 xl:py-8 xl:px-[250px] ">
-           <HeaderLogo/>
+export function Header() {
+  return (
+    <div className="top-0 z-10 bg-white w-[1258px] h-[57px] justify-between   flex gap-5 container mx-auto border px-5 py-5 xl:py-[8px]  xl:px-[24px] ">
+      <div className=" flex gap-8 justify-center items-center">
+        <HeaderLogo />
+        <h2 className="text-[#18BA51]">НҮҮР</h2>
+        <h2>ХООЛНЫ ЦЭС</h2>
+        <h2>ХҮРГЭЛТИЙН БҮС</h2>
+      </div>
+      <div className=" flex gap-5">
+        <div className="flex justify-center items-center">
+          <label className="input h-[35px] input-bordered flex items-center gap-2">
+            <SearchIcon />
+            <input type="text" className="grow" placeholder="Хайх" />
+          </label>
         </div>
-    )
+        <div className="flex gap-3  items-center ">
+          <SagsIcon />
+          <p>Сагс</p>
+        </div>
+        <div className="flex gap-3 items-center ">
+          <NevtrehIcon />
+          <p>Нэвтрэх</p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,12 +1,12 @@
-import { Header } from "../components/Header"
-import { Footer } from "@/components/Footer"
-
+import { Header } from "../components/Header";
+import { Footer } from "@/components/Footer";
 
 export default function Home() {
-  return  (
-  <div className="bg-red-500">
-    <Header/>
-    hello team, this is the homepage.
-    <Footer/>
-  </div>
-)}
+  return (
+    <div className="bg-red-500">
+      <Header />
+      hello team, this is the homepage.
+      <Footer />
+    </div>
+  );
+}
