b28387258212bb707986189dae6151cbfe7d45b2
dzone24 <23LP8019@nest.edu.mn>
2024-03-21 20:45:06 +0800

    user-profile fixed little bit

--- ui/src/app/user-profile/page.tsx
+++ ui/src/app/user-profile/page.tsx
@@ -8,7 +8,7 @@ import { EditIcon } from "../../../public/Icons/EditIcon";
 export default function UserProfile() {
   return (
     <>
-      <div className="flex flex-col gap-5 border-2 rounded mt-5 mb-5 justify-center items-center mx-auto w-2/6 p-5">
+      <div className="flex flex-col gap-5 border-2 rounded mt-5 mb-5 justify-center items-center mx-auto w-1/3 p-5">
         <img
           src="/images/profile-pic.jpg"
           alt="Profile Picture"
@@ -17,42 +17,53 @@ export default function UserProfile() {
         <h1 className="text-2xl">УгтахБаяр</h1>
       </div>
 
-      <div className="flex flex-col gap-5 border-2 rounded mt-5 mb-5 justify-center items-center mx-auto w-2/6 p-5">
-        <div className="flex">
-          <UserIcon />
-          <div>
-            <p>Таны нэр</p>
-            <p>УгтахБаяр</p>
+      <div className="flex flex-col gap-5 border-2 rounded mt-5 mb-5 justify-center items-center mx-auto w-1/3 p-5">
+          <div className="flex bg-slate-200 rounded p-2 w-full items-center">
+            <div className="flex bg-slate-200 rounded p-2 w-full items-center gap-3">
+              <UserIcon />
+              <div>
+                <p>Таны нэр</p>
+                <p>УгтахБаяр</p>
+              </div>
+            </div>
+            <div className="p-3">
+              <EditIcon />
+            </div>
           </div>
-          <EditIcon />
-        </div>
-        <div className="flex">
-          <PhoneIcon />
-          <div>
-            <p>Утасны дугаар</p>
-            <p>88883345</p>
+
+          <div className="flex bg-slate-200 rounded p-2 w-full items-center">
+            <div className="flex bg-slate-200 rounded p-2 w-full items-center gap-3">
+              <PhoneIcon />
+              <div>
+                <p>Утасны дугаар</p>
+                <p>88883345</p>
+              </div>
+            </div>
+            <div className="p-3">
+              <EditIcon />
+            </div>
           </div>
-          <EditIcon />
-        </div>
-        <div className="flex">
-          <EmailIcon />
-          <div>
-            <p>Имэйл хаяг</p>
-            <p>Ugtakhbayr@gmail.com</p>
+
+          <div className="flex bg-slate-200 rounded p-2 w-full items-center">
+            <div className="flex bg-slate-200 rounded p-2 w-full items-center gap-3">
+              <EmailIcon />
+              <div>
+                <p>Имэйл хаяг</p>
+                <p>Ugtakhbayr@gmail.com</p>
+              </div>
+            </div>
+            <div className="p-3">
+              <EditIcon />
+            </div>
+            
           </div>
-          <EditIcon />
-        </div>
-        <div className="flex">
+        <div className="flex  rounded p-2 w-full items-center gap-3">
           <TimeIcon />
-          <div>
-            <p>Захиалгын түүх</p>
-          </div>
+          <p>Захиалгын түүх</p>
         </div>
-        <div className="flex">
+        <div className="flex  rounded p-2 w-full items-center gap-3">
           <LogoutIcon />
-          <div>
-            <p>Гарах</p>
-          </div>
+          <p>Гарах</p>
         </div>
       </div>
     </>
