5fec57e8263378e2f1148eafc407607eda45557e
ganaa0419 <b.ganaa0419@gmail.com>
2024-03-21 08:49:04 +0800

    footer edited

---
+++ ui/src/components/Data/MenuItems.tsx
@@ -0,0 +1,8 @@
+export const menuItems = [
+    {id: '1', name: 'Нүүр', link: '/'},
+    {id: '2', name: 'Холбоо барих', link: '/contact-us'},
+    {id: '3', name: 'Хоолны цэс', link: '/menu'},
+    {id: '4', name: 'Үйлчилгээний нөхцөл', link: '/tos'},
+    {id: '5', name: 'Хүргэлтийн бүс', link: '/zone'},
+    {id: '6', name: 'Нууцлалын бодлого', link: '/privacy-policy'},
+]
--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from "../components/Header"
+import { Footer } from "@/components/Footer"
 
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
   <div className="bg-red-500">
     <Header/>
     hello team, this is the homepage.
+    <Footer/>
   </div>
 
 )}
