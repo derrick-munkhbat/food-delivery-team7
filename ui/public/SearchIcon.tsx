3bcefc7c25e4ca0c44859a472250cb1d85bb99df
Amgalanbaatar <amgaabra08@gmail.com>
2024-03-21 16:05:41 +0800

    Header

--- ui/public/SearchIcon.tsx
+++ ui/public/SearchIcon.tsx
@@ -1,15 +1,18 @@
 export function SearchIcon() {
   return (
     <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 16 16"
-      fill="currentColor"
-      className="w-4 h-4 opacity-70"
     >
       <path
-        fillRule="evenodd"
-        d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-        clipRule="evenodd"
+        d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
       />
     </svg>
   );
