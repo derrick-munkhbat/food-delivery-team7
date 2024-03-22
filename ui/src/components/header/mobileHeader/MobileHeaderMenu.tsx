7e009ab6611fe61db198018912fdb94558b9b8ce
Amgalanbaatar <amgaabra08@gmail.com>
2024-03-22 18:27:45 +0800

    header

--- ui/src/components/header/mobileHeader/MobileHeaderMenu.tsx
+++ ui/src/components/header/mobileHeader/MobileHeaderMenu.tsx
@@ -1,17 +1,15 @@
-// export function MobileHeadermenu() {
-//   return (
-//     <>
-//       <div className="flex  p-2 md:flex items-center justify-center flex-1 gap-10 ">
-//         {mainMenuItems.map((item) => (
-//           <a
-//             key={item.id}
-//             className="px-2 py4 border-l-violet-200"
-//             href={item.link}
-//           >
-//             {item.label}
-//           </a>
-//         ))}
-//       </div>
-//     </>
-//   );
-// }
+import { mobileMenuItems } from "./MobileMenuItems";
+
+export function MobileHeaderMenu() {
+  return (
+    <>
+      <div className="flex flex-col items-center gap-5 mt-4 font-bold font-sans border-t-4">
+        {mobileMenuItems.map((item) => (
+          <a key={item.id} className="px-2 py4 " href={item.link}>
+            {item.name}
+          </a>
+        ))}
+      </div>
+    </>
+  );
+}
