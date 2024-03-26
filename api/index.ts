f41baae1f02e8ec8d494ed2f6f2b6f189dec22b2
Гантулга Баттулга <23LP2023@G305-CC0077.local>
2024-03-26 19:02:44 +0800

    backend started

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+
+const connectionString = "mongodb+srv://bganaa0419:iELB55cozoCM8Gm2@food-delivery.ppokdxp.mongodb.net/?retryWrites=true&w=majority&appName=food-delivery"
+
+const connectDB = async () => {
+  try {
+    mongoose.connect(connectionString);
+    console.log("Connected");
+  } catch(error) {
+    console.log("Failed", error);
+  }
+}
+
+
 
 const app = express();
 
