e09444d21aaeac740ff17a080afc1233c5c8901c
dzone24 <23LP8019@nest.edu.mn>
2024-03-31 15:01:23 +0800

    cart
    changed card modal layout to drawer menu with transition

--- ui/src/components/header/cart-modal/CartHeader.tsx
+++ ui/src/components/header/cart-modal/CartHeader.tsx
@@ -5,12 +5,12 @@ export function CartHeader() {
   return (
     <div className="flex w-full h-18 items-center self-start border-b-2">
       <button
-        className="flex p-3 mb-5"
+        className="flex p-3"
         //   onClick={() => setIsShowModal(false)}
       >
         <ArrowIcon />
       </button>
-      <p className="flex w-full items-center justify-center text-2xl p-3 mb-5">
+      <p className="flex w-full items-center justify-center text-2xl p-3">
         Таны сагс
       </p>
     </div>
