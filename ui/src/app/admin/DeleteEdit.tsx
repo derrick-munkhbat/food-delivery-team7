3a3ab09b95760ec3509466201e7e65173204a284
Amgalanbaatar <amgaabra08@gmail.com>
2024-04-09 16:40:40 +0800

    last push

--- ui/src/app/admin/DeleteEdit.tsx
+++ ui/src/app/admin/DeleteEdit.tsx
@@ -3,20 +3,14 @@ import { MdModeEdit } from "react-icons/md";
 import { DetailIcon } from "../../components/icons/DetailIcon";
 import axios from "axios";
 import { useState } from "react";
-import { ChangeEventHandler, PropsWithChildren } from "react";
 
 type DeleteEditProps = {
-  categoryName: string;
   categoryId: string;
   onChange: () => void;
 };
 
-export function DeleteEdit({
-  categoryName,
-  categoryId,
-  onChange,
-}: DeleteEditProps) {
-  const [name, setName] = useState(categoryName);
+export function DeleteEdit({ categoryId, onChange }: DeleteEditProps) {
+  const [name, setName] = useState("");
 
   // ----DELETE CATEGORY----//
   async function handleDeleteCategory(id: string) {
@@ -33,74 +27,38 @@ export function DeleteEdit({
   }
   // ----UPDATE CATEGORY----//
   async function handleEditCategory(id: string) {
-    //   {
-    //     try {
-    //       await axios.put(`http://localhost:8000/category/${id}`).then(() => {});
-    //     } catch (error) {
-    //       console.log(error);
-    //     }
-    //   }
-    // }
-    document.getElementById(id).showModal();
-    console.log(name);
-
-    return (
-      <div className="dropdown dropdown-right dropdown-end">
-        <div tabIndex={0} className="m-1  p-2">
-          <DetailIcon />
-        </div>
-        <ul
-          tabIndex={0}
-          className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-        >
-          <li onClick={() => handleEditCategory(categoryId)}>
-            <div>
-              <MdModeEdit />
-              <a>Edit name</a>
-            </div>
-          </li>
-
-          <li onClick={() => handleDeleteCategory(categoryId)}>
-            <div className="text-[#DF1F29]">
-              <FaRegTrashAlt />
-              <a className="text-[#DF1F29]">Delete Category </a>
-            </div>
-          </li>
-        </ul>
-        <dialog id="my_modal_3" className="modal">
-          <div className="modal-box">
-            <form method="dialog">
-              {/* if there is a button in form, it will close the modal */}
-              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-                ✕
-              </button>
-            </form>
-            <h3 className="font-bold text-lg justify-center flex items-center">
-              Edit category
-            </h3>
-            <div className="divider m-0"></div>
-            <div className="">
-              <p className="py-4">Category name</p>
-              <input
-                value={name}
-                onChange={(event) => setName(event.target.value)}
-                type="text"
-                placeholder="Type here"
-                className="input w-[100%] input-bordered max-w-xs"
-              />
-            </div>
-            <div className="flex items-center gap-5 ml-[65%]">
-              <button>Clear</button>
-              <button
-                className="w-[109px] h-[40px] border px-[16px] py-[10px] rounded bg-[#393939] flex items-center text-white"
-                onClick={() => handleEditCategory}
-              >
-                Continue
-              </button>
-            </div>
-          </div>
-        </dialog>
-      </div>
-    );
+    if (window.prompt("Edit", name)) {
+      try {
+        await axios.put(`http://localhost:8000/category/${id}`).then(() => {});
+      } catch (error) {
+        console.log(error);
+      }
+    }
   }
+
+  return (
+    <div className="dropdown dropdown-right dropdown-end">
+      <div tabIndex={0} className="m-1  p-2">
+        <DetailIcon />
+      </div>
+      <ul
+        tabIndex={0}
+        className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+      >
+        <li onClick={() => handleEditCategory(categoryId)}>
+          <div>
+            <MdModeEdit />
+            <a>Edit name</a>
+          </div>
+        </li>
+
+        <li onClick={() => handleDeleteCategory(categoryId)}>
+          <div className="text-[#DF1F29]">
+            <FaRegTrashAlt />
+            <a className="text-[#DF1F29]">Delete Category </a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
