d58251386540b2db1feea28bb6b9bc8c1f50f2a1
GantogtokhC <23LP4408@nest.edu.mn>
2024-04-15 18:51:02 +0800

    login sign-up completed

--- api/controller/user.controller.ts
+++ api/controller/user.controller.ts
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import { UserModel } from "../models/user.models";
+import bcrypt from "bcrypt";
 
 export async function getUser(_req: any, res: any) {
   const user = await UserModel.find();
@@ -9,18 +10,33 @@ export async function getUser(_req: any, res: any) {
 
 export async function createUser(req: any, res: any) {
   const {userName , userEmail , userPassword} = req.body
-  const user = await UserModel.create({
+
+  //hereglegciin email shalgah
+  const existingUser = await UserModel.findOne({ userEmail });
+  if (existingUser) return res.status(400).send('Email already exists');
+
+  //password 
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(userPassword, salt);
+
+
+  const newUser = await UserModel.create({
     userName,
     userEmail,
-    userPassword,
+    userPassword: hashedPassword,
   });
-
-  res.json(user);
+  res.sendStatus(201).send('welcome')
+  res.json(newUser);
 }
 export async function loginUser(req:any, res:any) {
   const {userEmail , userPassword} = req.body;
 
-  const user = await UserModel.find();
+  const user = await UserModel.findOne({userEmail });
+  if(!user) return  res.sendStatus(400).send('invalid email')
+  
+ 
+  const validPassword = await bcrypt.compare(userPassword , `${user.userPassword}`);
+  if (!validPassword) return res.status(400).send('Invalid password');
 
   const loggedIn = true;
 if (loggedIn) {
@@ -28,5 +44,18 @@ if (loggedIn) {
   res.json({ accessToken });
 }
 res.sendStatus(401);
+
+//   const {userEmail , userPassword} = req.body;
+
+//   const user = await UserModel.findOne({userEmail});
+//   if(!user) return  res.sendStatus(400).send('invalid email')
+//   // const validPassword = await UserModel.findOne({userPassword});
+
+//   const loggedIn = true;
+// if (loggedIn) {
+//   const accessToken = jwt.sign({ userEmail: userEmail }, "secret_string123");
+//   res.json(user, { accessToken });
+// }
+// res.sendStatus(401);
 }
 
