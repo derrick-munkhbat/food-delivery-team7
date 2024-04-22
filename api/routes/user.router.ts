ef7fca3efb093a7ee6dc467a834e5ea6a62c8bba
GantogtokhC <23LP4408@nest.edu.mn>
2024-04-22 19:10:03 +0800

    adminchecker

--- api/routes/user.router.ts
+++ api/routes/user.router.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUser, createUser, loginUser } from "../controller/user.controller";
+import { getUser, createUser, loginUser, updateUser } from "../controller/user.controller";
 
 const userRouter = express.Router();
 
