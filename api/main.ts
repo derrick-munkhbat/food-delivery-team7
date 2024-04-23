8097cf3e1390333532ba1b0176b95a7f4e3fa3ef
hi <hihi@his-Laptop.local>
2024-04-23 16:59:41 +0800

    orderUpdate

--- api/main.ts
+++ api/main.ts
@@ -23,6 +23,7 @@ app.use("/order", orderRouter);
 app.get("/", (req, res) => {
   res.send("hello team, the backend is running");
 });
+app.use("/orderItems", orderRouter);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
