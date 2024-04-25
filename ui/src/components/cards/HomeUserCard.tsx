0c2042f9bec4ad09adc119f1bf1f4085ddc92f99
GantogtokhC <23LP4408@nest.edu.mn>
2024-04-25 18:43:02 +0800

    fixed

--- ui/src/components/cards/HomeUserCard.tsx
+++ ui/src/components/cards/HomeUserCard.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+"use client"
+import { useEffect, useState } from "react";
 import { useFood } from "@/app/globals";
 import { NumericFormat } from "react-number-format";
 import { OrderModal } from "@/app/menu/order";
@@ -6,64 +7,133 @@ import { OrderModal } from "@/app/menu/order";
 export function HomeUserCard() {
   const { foods }: any = useFood();
   const [foodId, setFoodId] = useState("");
+  const [token, setToken] = useState<string | null>(null);
 
-  return (
-    <div className="grid grid-cols-1 xl:grid-cols-4 sm:grid-cols-2 justify-items-center gap-[60px] mt-10 mb-5">
-      {foods.map((food: any) => (
-        <div
-          className="grid gap-[14px]"
-          key={food._id}
-          onClick={() => setFoodId(food._id)}
-        >
-          <div className="w-[282px]">
-            <div className="bg-[url('/images/menuZurag.jpg')] grid items-center justify-items-center group-hover:opacity-60 h-[186px]  duration-300  rounded-2xl ease-in-out  bg-center w-[282px] relative">
-              {food.sales > 0 && (
-                <div className="border-2 border-white text-white bg-[#18BA51] rounded-2xl py-1 px-4 absolute top-4 right-4 font-semibold text-lg">
-                  {food.sales}%
-                </div>
-              )}
+  
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    setToken(token)
+  }, []);
+  if(token){
+    return (
+      <div className="grid grid-cols-1 xl:grid-cols-4 sm:grid-cols-2 justify-items-center gap-[60px] mt-10 mb-5">
+        {foods.map((food: any) => (
+          <div
+            className="grid gap-[14px]"
+            key={food._id}
+            onClick={() => setFoodId(food._id)}
+          >
+            <div className="w-[282px]">
+              <div className="bg-[url('/images/menuZurag.jpg')] grid items-center justify-items-center group-hover:opacity-60 h-[186px]  duration-300  rounded-2xl ease-in-out  bg-center w-[282px] relative">
+                {food.sales > 0 && (
+                  <div className="border-2 border-white text-white bg-[#18BA51] rounded-2xl py-1 px-4 absolute top-4 right-4 font-semibold text-lg">
+                    {food.sales}%
+                  </div>
+                )}
+              </div>
             </div>
-          </div>
-
-          <div className="gap-1">
-            <p className="text-xl font-semibold text-black">{food.name}</p>
-            <p className="text-lg font-semibold text-[#18BA51] ">
-              {food.sales > 0 && (
-                <div className="flex">
+  
+            <div className="gap-1">
+              <p className="text-xl font-semibold text-black">{food.name}</p>
+              <p className="text-lg font-semibold text-[#18BA51] ">
+                {food.sales > 0 && (
+                  <div className="flex">
+                    <NumericFormat
+                      value={food.price - (food.sales / 100) * food.price}
+                      allowLeadingZeros
+                      thousandSeparator=","
+                      suffix="₮"
+                      className="bg-transparent text-lg font-semibold w-20"
+                      disabled
+                    />
+                    <NumericFormat
+                      value={food.price}
+                      allowLeadingZeros
+                      thousandSeparator=","
+                      suffix="₮"
+                      className="bg-transparent text-sm text-black line-through w-20"
+                      disabled
+                    />
+                  </div>
+                )}
+                {food.sales === 0 && (
                   <NumericFormat
-                    value={food.price - (food.sales / 100) * food.price}
+                    value={food.price}
                     allowLeadingZeros
                     thousandSeparator=","
                     suffix="₮"
                     className="bg-transparent text-lg font-semibold w-20"
                     disabled
                   />
+                )}
+              </p>
+            </div>
+          </div>
+        ))}
+  
+        <OrderModal foodId={foodId} onClose={() => setFoodId("")} />
+      </div>
+    );
+  }else{
+    return (
+      <div className="grid grid-cols-1 xl:grid-cols-4 sm:grid-cols-2 justify-items-center gap-[60px] mt-10 mb-5">
+        {foods.map((food: any) => (
+          <div
+            className="grid gap-[14px]"
+            key={food._id}
+            onClick={() => window.location.href="/sign-in"}
+          >
+            <div className="w-[282px]">
+              <div className="bg-[url('/images/menuZurag.jpg')] grid items-center justify-items-center group-hover:opacity-60 h-[186px]  duration-300  rounded-2xl ease-in-out  bg-center w-[282px] relative">
+                {food.sales > 0 && (
+                  <div className="border-2 border-white text-white bg-[#18BA51] rounded-2xl py-1 px-4 absolute top-4 right-4 font-semibold text-lg">
+                    {food.sales}%
+                  </div>
+                )}
+              </div>
+            </div>
+  
+            <div className="gap-1">
+              <p className="text-xl font-semibold text-black">{food.name}</p>
+              <p className="text-lg font-semibold text-[#18BA51] ">
+                {food.sales > 0 && (
+                  <div className="flex">
+                    <NumericFormat
+                      value={food.price - (food.sales / 100) * food.price}
+                      allowLeadingZeros
+                      thousandSeparator=","
+                      suffix="₮"
+                      className="bg-transparent text-lg font-semibold w-20"
+                      disabled
+                    />
+                    <NumericFormat
+                      value={food.price}
+                      allowLeadingZeros
+                      thousandSeparator=","
+                      suffix="₮"
+                      className="bg-transparent text-sm text-black line-through w-20"
+                      disabled
+                    />
+                  </div>
+                )}
+                {food.sales === 0 && (
                   <NumericFormat
                     value={food.price}
                     allowLeadingZeros
                     thousandSeparator=","
                     suffix="₮"
-                    className="bg-transparent text-sm text-black line-through w-20"
+                    className="bg-transparent text-lg font-semibold w-20"
                     disabled
                   />
-                </div>
-              )}
-              {food.sales === 0 && (
-                <NumericFormat
-                  value={food.price}
-                  allowLeadingZeros
-                  thousandSeparator=","
-                  suffix="₮"
-                  className="bg-transparent text-lg font-semibold w-20"
-                  disabled
-                />
-              )}
-            </p>
+                )}
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
-
-      <OrderModal foodId={foodId} onClose={() => setFoodId("")} />
-    </div>
-  );
+        ))}
+  
+        {/* <OrderModal foodId={foodId} onClose={() => setFoodId("")} /> */}
+      </div>
+    );
+  }
 }
