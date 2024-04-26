9509ae94923a32848697013ebfdc86206c12257c
Amgalanbaatar <amgaabra08@gmail.com>
2024-04-26 16:54:08 +0800

    home

--- ui/src/components/header/headerMenuMaps/userMenu.tsx
+++ ui/src/components/header/headerMenuMaps/userMenu.tsx
@@ -10,24 +10,24 @@ import axios from "axios";
 
 import Basket from "@/app/basket/page";
 export function UserMenu() {
-  const { user, login } = useUser()
+  const { user, login } = useUser();
   const [token, setToken] = useState<string | null>(null);
 
   useEffect(() => {
     const token = localStorage.getItem("accessToken");
-    setToken(token)
+    setToken(token);
     const getUser = async () => {
       if (!token) {
-        return
+        return;
       }
-      const { data } = await axios.get('http://localhost:8000/user', {
+      const { data } = await axios.get("http://localhost:8000/user", {
         headers: {
-          accessToken: token
-        }
-      })
-    }
-    getUser()
-  }, [])
+          accessToken: token,
+        },
+      });
+    };
+    getUser();
+  }, []);
 
   const pathname = usePathname();
   if (token) {
@@ -43,48 +43,54 @@ export function UserMenu() {
 
         <div className="flex  p-2 md:flex items-center justify-center flex-1 gap-10 font-bold text-base ">
           <div className="flex justify-center items-center">
-          <UserIcon/>
-          <a className={`px-2 py4 border-l-violet-200 ${
-              pathname === "/user-profile" ? "text-[#18BA51]" : ""
-            }`} href="/user-profile">Хэрэглэгч</a>
+            <UserIcon />
+            <a
+              className={`px-2 py4 border-l-violet-200 ${
+                pathname === "/user-profile" ? "text-[#18BA51]" : ""
+              }`}
+              href="/user-profile"
+            >
+              Хэрэглэгч
+            </a>
           </div>
+        </div>
       </div>
-    </div>
-    )
-  }else{
+    );
+  } else {
     return (
       <div className="flex gap-5">
-        <div className="flex gap-3 font-bold  justify-center flex-1 max-sm:hidden  items-center  ">
+        {/* <div className="flex gap-3 font-bold  justify-center flex-1 max-sm:hidden  items-center  ">
           <label className="input h-[35px] input-bordered flex items-center gap-2">
             <SearchIcon />
             <input type="text" className="grow" placeholder="Хайх" />
           </label>
           <Basket />
         </div>
-  
+   */}
         <div className="flex gap-5">
-        <div className="flex gap-3 font-bold  justify-center flex-1 max-sm:hidden  items-center  ">
-          <label className="input h-[35px] input-bordered flex items-center gap-2">
-            <SearchIcon />
-            <input type="text" className="grow" placeholder="Хайх" />
-          </label>
-          <Basket />
-        </div>
-  
-        <div className="flex  p-2 md:flex items-center justify-center flex-1 gap-10 font-bold text-base ">
-        <div className="flex justify-center items-center">
-          <UserIcon/>
-          <a className={`px-2 py4 border-l-violet-200 ${
-              pathname === "/sign-in" ? "text-[#18BA51]" : ""
-            }`} href="/sign-in">Нэвтрэх</a>
+          <div className="flex gap-3 font-bold  justify-center flex-1 max-sm:hidden  items-center  ">
+            <label className="input h-[35px] input-bordered flex items-center gap-2">
+              <SearchIcon />
+              <input type="text" className="grow" placeholder="Хайх" />
+            </label>
+            <Basket />
           </div>
-          
+
+          <div className="flex  p-2 md:flex items-center justify-center flex-1 gap-10 font-bold text-base ">
+            <div className="flex justify-center items-center">
+              <UserIcon />
+              <a
+                className={`px-2 py4 border-l-violet-200 ${
+                  pathname === "/sign-in" ? "text-[#18BA51]" : ""
+                }`}
+                href="/sign-in"
+              >
+                Нэвтрэх
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
       </div>
     );
   }
-
-
- 
 }
